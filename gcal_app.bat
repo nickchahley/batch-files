9a955d3c9f667138c051a9d5e6f0c21d85eaa84d
nickchahley <nickchahley@gmail.com>
2020-03-24 12:18:42 -0400

    Do not keep a cmd window open after starting program

--- gcal_app.bat
+++ gcal_app.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory=Default --app=http://calendar.google.com
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory=Default --app=http://calendar.google.com
